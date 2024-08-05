a1d4c2c1d9dd5377c56a5e2093e80bfce5891ab4
JoelLemuelAbuga <jlabuga2k16@gmail.com>
2024-08-05 15:30:45 +0800

    updated product id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "4292319871110";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";

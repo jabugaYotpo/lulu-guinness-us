109eec0d36080f26594eb49e0455c14abb0a3010
AzureAD\JoelLemuelAbuga <jlabuga2k16@gmail.com>
2024-08-26 23:07:17 +0800

    ripcurl UK

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/aBANMJVQZNFxxijeRjwNKTJbVcPcUH2coQBuOk8e" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/bkBnw8SBNTCtaTOEv3Jr42GvAjOOmDIO9BvzIhsI" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 

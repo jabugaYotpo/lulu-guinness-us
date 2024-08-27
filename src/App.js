db504bed0367f67bac9cdec867f7d673ea4c21ac
AzureAD\JoelLemuelAbuga <jlabuga2k16@gmail.com>
2024-08-27 15:43:07 +0800

    Ripcurl netherlands

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "840544";
+  let InstanceId_Widget = "841136";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "840545";
+  let InstanceId_StarRating = "841137";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/XS0B4iBqvPG78R4moI0pUmNUYKPAUVWJmmSuVTfw" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/C92S6Kl36rjipyjAn7ZbXPexh5UPAIkAOpu4hQ4B" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 

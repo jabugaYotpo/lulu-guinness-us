3f2ab54a75e1057900a74cf6e92d0cf840e54793
AzureAD\JoelLemuelAbuga <jlabuga2k16@gmail.com>
2025-01-28 02:23:17 +0800

    artilect

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import UgcGallery from "./components/UgcGallery";
 
 function App() {
   //Instance ID
-  let InstanceId_Widget = "915213";
+  let InstanceId_Widget = "814439";
   let InstanceId_QA = "";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
-  let InstanceId_StarRating = "";
+  let InstanceId_StarRating = "1012052";
   let InstanceId_PromotedProd = "";
   let InstanceId_UgcGallery = "";
   let InstanceId_ReviewsTab = "";
